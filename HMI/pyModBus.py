550731d73324e1d8470fa5356e4c6ee52d0fe70c
Adam <addycakes17@gmail.com>
2021-02-08 08:41:40 -0500

    Add files via upload

--- HMI/pyModBus.py
+++ HMI/pyModBus.py
@@ -1,20 +1,20 @@
 from pymodbus.client.sync import ModbusSerialClient
 
 # port="/dev/ttyACM0"
-client = ModbusSerialClient(method='ascii', port="/dev/ttyUSB0", stopbits=1, bytesize=8, baudrate=9600, parity='N', timeout=1)
+client = ModbusSerialClient(method='ascii', port="/dev/ttyUSB0", stopbits=1, bytesize=8, baudrate=38400, parity='O', timeout=1)
 
 connection = client.connect()
 if connection:print "connected"
 
 result = client.read_coils(1,6,unit=0x01)
+parts = client.read_coils(7,8,unit=0x01)
 
-f = open('ascii.txt', 'r')
-print f.read()
-f.close()
+d = open('parts.txt', 'w')
+d.write(parts)
+d.close()
 
 f = open('ascii.txt', 'w')
 f.write(result)
-print result
 f.close()
 
 client.close()
